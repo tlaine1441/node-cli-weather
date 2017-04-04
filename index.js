c04ea9bcc434a1a4871eec00ed272bb74b71e86e
Taylor Laine <tlaine@msudenver.edu>
2017-04-04 15:13:56 -0600

    done

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+'use strict'
 //Bonus: take in state and city as arguments instead of hardcoding
 var state = "CO";
 var city = "Denver";
 
 //Require apiKey
-
+const apiKey = require('./env');
 
 //Require the request library
+
+const request = require('request');
 //What is the `request` library you ask?
 //Google `npm request` and you can find out!
-
+const test = request("http://api.wunderground.com/api/" + apiKey.key + "/conditions/q/CO/Denver.json", function(err, response, body) {
+	const weather = JSON.parse(body);
+	console.log("The weather in " + weather.current_observation.display_location.city + " is " + weather.current_observation.weather);
+});
+	
 
 // Set the API URL we are hitting, with our API Key, state, and city
 
